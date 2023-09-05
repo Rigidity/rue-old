0d3997bf9638291b376a8b1eecfc68a4d9052a40
Rigidity <me@rigidnetwork.com>
2023-09-05 19:09:39 -0400

    Move temp compiler code

--- crates/rue-compiler/src/lib.rs
+++ crates/rue-compiler/src/lib.rs
@@ -1,1 +1,22 @@
+use rue_parser::{FnDef, Item, Program};
 
+pub fn enter_program(program: Program) {
+    println!("Entering program");
+
+    for item in program.items() {
+        enter_item(item);
+    }
+}
+
+fn enter_item(item: Item) {
+    println!("Entering item");
+
+    match item {
+        Item::FnDef(fn_def) => enter_fn_def(fn_def),
+    }
+}
+
+fn enter_fn_def(fn_def: FnDef) {
+    println!("Entering fn def");
+    println!("Name = {:?}", fn_def.name());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rowan::ast::AstNode;
+use rue_compiler::enter_program;
 use rue_lexer::Lexer;
-use rue_parser::{FnDef, Item, Parser, Program};
+use rue_parser::{Parser, Program};
 use rue_syntax::SyntaxNode;
 
 fn main() {
@@ -17,25 +18,4 @@ fn main() {
 
     let ast = Program::cast(node).unwrap();
     enter_program(ast);
-}
-
-fn enter_program(program: Program) {
-    println!("Entering program");
-
-    for item in program.items() {
-        enter_item(item);
-    }
-}
-
-fn enter_item(item: Item) {
-    println!("Entering item");
-
-    match item {
-        Item::FnDef(fn_def) => enter_fn_def(fn_def),
-    }
-}
-
-fn enter_fn_def(fn_def: FnDef) {
-    println!("Entering fn def");
-    println!("Name = {:?}", fn_def.name());
 }
