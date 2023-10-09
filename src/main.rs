fef3e064728b720ba222eb2fa6ca9d4d97d20799
Rigidity <me@rigidnetwork.com>
2023-10-08 22:03:41 -0400

    Fix output

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,6 @@ use std::fs;
 
 use anyhow::anyhow;
 use clap::Parser;
-use clvmr::{
-    reduction::Reduction,
-    serde::{node_from_bytes, node_to_bytes},
-    Allocator, ChiaDialect,
-};
 use rowan::ast::AstNode;
 use rue_ast::Program;
 use rue_compiler::Compiler;
@@ -28,25 +23,8 @@ fn main() -> anyhow::Result<()> {
         .map_err(|_| anyhow!("unknown file {}", args.input_file))?;
 
     match compile(&source) {
-        Ok(bytes) => {
-            println!("{}", hex::encode(&bytes));
-
-            let mut a = Allocator::new();
-            let ptr = node_from_bytes(&mut a, &bytes).unwrap();
-            let dialect = ChiaDialect::new(0);
-            let nil = a.null();
-
-            match clvmr::run_program(&mut a, &dialect, ptr, nil, u64::MAX) {
-                Ok(Reduction(cost, result)) => println!(
-                    "\n= {} ({cost} cost)",
-                    hex::encode(node_to_bytes(&a, result).unwrap())
-                ),
-                Err(error) => println!("{error}"),
-            }
-        }
-        Err(errors) => {
-            eprintln!("{:?}", errors);
-        }
+        Ok(bytes) => println!("{}", hex::encode(&bytes)),
+        Err(errors) => eprintln!("{:?}", errors),
     }
 
     Ok(())
